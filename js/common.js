935d589c97281eb3c316435aabf0a969aa46bb38
Oreonax <esok@mailoo.org>
2016-12-19 11:20:04 +0100

    Added ajax form-contact

--- js/common.js
+++ js/common.js
@@ -5,7 +5,23 @@ $(document).ready(function(){
 	  var $form = $(this);
 
 	  var posting = $.post($form.attr("action"), $form.serialize())
- 
+
+	  // Put the results in a div
+	  posting.done(function() {
+	    $('.w-form-done').show()
+	  });
+	  posting.fail(function() {
+	    $('.w-form-fail').show()
+	  });
+
+	});
+	$("#email-form-contact").submit(function(e) {
+	  e.preventDefault();
+
+	  var $form = $(this);
+
+	  var posting = $.post($form.attr("action"), $form.serialize())
+
 	  // Put the results in a div
 	  posting.done(function() {
 	    $('.w-form-done').show()
