70b8f4fc44f4082ef1c0c42abbaa3f9a7504a0a8
Adrien Bigler <adrien.bigler@heig-vd.ch>
2017-01-19 10:07:58 +0100

    Display map of venues on the home page

--- js/common.js
+++ js/common.js
@@ -64,4 +64,46 @@ $(document).ready(function(){
 			});
 	});
 
+	mapManagement.init()
+
 })
+
+var mapManagement = {
+	map: "",
+	init: function() {
+	  mapManagement.map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: 46.8127598, lng: 7.999},
+	    zoom: 6
+	  });
+	  mapManagement.placeMarker()
+	},
+	placeMarker: function(){
+		var lang = "en"
+		if(window.location.href.indexOf("/fr/") > -1) {
+       lang = "fr"
+    }
+    if(window.location.href.indexOf("/de/") > -1) {
+       lang = "de"
+    }
+		$.getJSON( "mapdata/venues.json", function( data ) {
+		  $.each( data, function( key, val ) {
+		  	var myLatLng = {lat: val.lat, lng: val.lng};
+			  var marker = new google.maps.Marker({
+			    position: myLatLng,
+			    map: mapManagement.map,
+			    title: 'Hello World!'
+			  });
+			  var title = "<h1>"+val.title_en+"</h1>"
+			  if(lang == "fr"){
+			  	var title = "<h1>"+val.title+"</h1>"
+			  }
+		    var infowindow = new google.maps.InfoWindow({
+			    content: title
+			  });
+			  marker.addListener('click', function() {
+		    	infowindow.open(mapManagement.map, marker);
+		  	});
+		  });
+		});
+	}
+}
