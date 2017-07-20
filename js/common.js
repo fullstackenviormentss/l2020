3718383ff1db8a0fd1dd1d0a4bd8c07f5774dfe3
Mathieu Carrard <carrardm@gmail.com>
2017-07-20 15:58:27 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -440,14 +440,14 @@ var mapManagement = {
           position: myLatLng,
           map: mapManagement.map,
           icon: '/images/venues/location_v2.png',
-          title: 'Hello World!'
+          title: 'Click!'
         });
         var content = "<h1>" + val.title_en + "</h1><p>" + val.texte_en + "</p><p><a href=" + val.lienEn + " target='_blank'>More</a></p>";
         if (lang == "fr") {
           var content = "<h1>" + val.title + "</h1><p>" + val.texte_fr + "</p><p><a href=" + val.link + " target='_blank'>En savoir plus</a></p>";
         }
         if (lang == "de") {
-          var content = "<h1>" + val.title_de + "</h1><p>" + val.texte_de + "</p><p><a href=" + val.lienDe + " target='_blank'>Mehr infos</a></p>";
+          var content = "<h1>" + val.title_de + "</h1><p>" + val.texte_de + "</p><p><a href=" + val.lienDe + " target='_blank'>Mehr Infos</a></p>";
         }
         var infowindow = new google.maps.InfoWindow({
           content: content
