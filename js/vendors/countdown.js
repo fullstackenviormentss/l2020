a8236a0ae3a7f3b683a9b6ccec9bf8b80d0028e7
Mathieu Carrard <carrardm@gmail.com>
2018-01-12 13:47:33 +0100

    Add files via upload

--- js/vendors/countdown.js
+++ js/vendors/countdown.js
@@ -2,12 +2,41 @@
 /* the js */
 
 (function($){
+	function windowResize(){
+	    var scale = scaleX = scaleY = 1;
+	    var wWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || document.body.offsetWidth;
+	    var wHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || document.body.offsetHeight;
+	    var adWidth = jQuery('#countdown_container_tag').width();
+	    var adHeight = jQuery('#countdown_container_tag').height();
+	    if(adWidth > wWidth){
+	        scaleX = wWidth/adWidth;
+	    }
+	    if(adHeight > wHeight){
+	        scaleY = wHeight/adHeight;
+	    }
+	    if(scaleX < scaleY){
+	        scale = scaleX;
+	    }else{
+	        scale = scaleY;
+	    }
+	    var x = (wWidth - adWidth)/scale;
+	    var y = (wHeight - adHeight)/scale;
+	    var newHeight = scale * adHeight;
+	    jQuery('#countdown_container_tag').css('transform', 'scale(' + scale + ')');
+	    jQuery('#countdown_container_tag').css('-o-transform', 'scale(' + scale + ')');
+	    jQuery('#countdown_container_tag').css('-moz-transform', 'scale(' + scale + ')');
+	    jQuery('#countdown_container_tag').css('-webkit-transform', 'scale(' + scale + ')');
+	    jQuery('.border').css('height',newHeight);
+	}
+
+    window.addEventListener("resize", windowResize);
+    window.addEventListener("load", windowResize);
 	$.fn.countdown = function(custom_options)
 	{
 		var options = {
-			'lang': "fr"
+			'lang': "en"
 			// 'timestamp': integer
-		};
+		}
 
 		//var timestamp_url = './buenos-aires.php?callback=var%20data%20=';
 		var timestamp_url = 'https://countdown.omegawatches.com/minisites/buenosaires2018/buenos-aires.php';
@@ -18,17 +47,17 @@
                 number = "0" + number;
             }
             return number;
-        };
+        }
 
 		var local_get_time = function(tz_offset) {
 			var utc = new Date().getTime() / 1000;
 			var local = utc - tz_offset * 60;
 			return local;
-		};
+		}
 		var utc_get_time = function() {
 			var utc = new Date().getTime() / 1000;
 			return utc;
-		};
+		}
 
 		var translations = {
 			'de': {
@@ -49,8 +78,8 @@
 				'days': "JOURS",
 				'day': "JOUR",
 				'hours': "HRS",
-				'minutes': "MINS",
-				'seconds': "SECS"
+				'minutes': "MIN",
+				'seconds': "SEC"
 			},
 			'es': {
 				'days': "D&Iacute;AS",
@@ -61,7 +90,7 @@
 			}
 
 
-		};
+		}
 		$.extend(options, custom_options);
 		translations = translations[options.lang];
 
@@ -92,17 +121,15 @@
                 //var timeDiff = Math.abs(target_time.getTime() - current_time.getTime());
                 var timeDiff = target - current_utc;
                 
-                if (timeDiff <= 0 && timeDiff > -10) {
+                if (timeDiff <= 0 && timeDiff > -2) {
 					// blink mode
-					if ($all_countdown_digits.is(":visible")) {
-						$all_countdown_digits.hide(0);
-					} else {
-						$all_countdown_digits.show(0);
-					}
+					// if ($all_countdown_digits.is(":visible")) {
+					// 	$all_countdown_digits.hide(0);
+					// } else {
+					// 	$all_countdown_digits.show(0);
+					// }
                 } else if (timeDiff <= 0) {
 					// clock mode
-					$clock.show(0);
-					$countdown.hide(0);
 					var current = local_get_time(tz_offset);
                     var hours = Math.ceil((current / (3600)) % 24) - 1;
                     var minutes = Math.ceil((current / (60)) % 60) - 1;
@@ -110,9 +137,16 @@
 	                $clock_hours.html(to_nice_number(hours));
 	                $clock_minutes.html(to_nice_number(minutes));
 	                $clock_seconds.html(to_nice_number(seconds));
+					// $clock.show(0);
+					// $countdown.hide(0);
+					setTimeout(function(){ 
+	    				$countdown.css('opacity',0);
+	    				$clock.css('opacity',1);
+						// $clock.css(0);
+						// $countdown.hide(0);
+					}, 1000);
                 } else {
                     // countdown mode
-					$countdown.show(0);
 					var diffDays = Math.ceil(timeDiff / (3600 * 24)) - 1;
                     var diffHours = Math.ceil((timeDiff / (3600)) % 24) - 1;
                     var diffMinutes = Math.ceil((timeDiff / (60)) % 60) - 1;
@@ -121,13 +155,17 @@
 	                $hours.html(to_nice_number(diffHours));
 	                $minutes.html(to_nice_number(diffMinutes));
 	                $seconds.html(to_nice_number(diffSeconds));
-					if (diffDays == 1) {
+					if (diffDays <= 1) {
 						_self.find(".days_label").html(translations.day);
 					} else if (diffDays == 0) {
 						_self.find(".days_label").html(translations.days);
 					}
+					setTimeout(function(){ 
+	    				$countdown.css('opacity',1);
+						// $countdown.show(0);
+					}, 1000);
                 }
-            };
+            }
 
 			if (options.timestamp) {
 				target = options.timestamp;
@@ -143,7 +181,8 @@
 	                    console.log("could not load timestamp for countdown!");
 	                },
 	                success: function() {
-	                    target = new Date(data.targetDate).getTime();
+	                    // target = new Date(data.targetDate).getTime();
+	                    target = new Date(1578596400).getTime();
 						update_remaining();
 	                    setInterval(update_remaining, 1000);
 	                }
@@ -151,4 +190,5 @@
 			}
 		});
 	}
+
 })(jQuery);
